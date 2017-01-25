e89392e070b26fa30ea7b65802f36d399779ce3c
somkiat <somkiat.p@gmail.com>
2017-01-25 21:43:25 +0700

    For ying

--- app/src/main/java/com/workshop/calculator/CalculatorInteractor.java
+++ app/src/main/java/com/workshop/calculator/CalculatorInteractor.java
@@ -1,9 +1,15 @@
 package com.workshop.calculator;
 
+import android.util.Log;
+
 import com.workshop.calculator.network.CalculatorAPI;
 
+import java.io.IOException;
 import java.lang.annotation.Annotation;
 
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,9 +22,23 @@ interface CalculatorListener {
     void onSuccess(String result);
 }
 
+class JsonInterceptor implements Interceptor {
+    @Override
+    public okhttp3.Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();
+
+        okhttp3.Response response = chain.proceed(request);
+        String rawJson = response.body().string();
+
+        // Re-create the response before returning it because body can be read only once
+        return response.newBuilder()
+                .body(ResponseBody.create(response.body().contentType(), rawJson)).build();
+    }
+}
+
 public class CalculatorInteractor {
 
-    public static String CALCULATOR_ENDPOINT = "http://10.0.0.75:8882/";
+    public static String CALCULATOR_ENDPOINT = "http://10.0.2.2:8882/";
     private Retrofit retrofit;
     private CalculatorListener calculatorListener;
 
@@ -28,27 +48,48 @@ public class CalculatorInteractor {
 
 
     public void plus(int firstNumber, int secondNumber) {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(new JsonInterceptor());
         retrofit = new Retrofit.Builder()
                 .baseUrl(CALCULATOR_ENDPOINT)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
                 .build();
+
         CalculatorAPI calculatorAPI = retrofit.create(CalculatorAPI.class);
-        final Call<CalculatorAPI.Result> call =
-                calculatorAPI.plus(firstNumber, secondNumber);
+//        final Call<CalculatorAPI.Result> call =
+//                calculatorAPI.plus(firstNumber, secondNumber);
 
-        call.enqueue(new Callback<CalculatorAPI.Result>() {
+        CalculatorAPI.MyRequest myRequest = new CalculatorAPI.MyRequest("somkiat", "xxxx");
+        final Call<CalculatorAPI.UserInformation> call =
+                calculatorAPI.getByUser(21, myRequest);
 
+        call.enqueue(new Callback<CalculatorAPI.UserInformation>() {
             @Override
-            public void onResponse(Call<CalculatorAPI.Result> call, Response<CalculatorAPI.Result> response) {
-                String result = response.body().getResult();
-                calculatorListener.onSuccess(result);
+            public void onResponse(Call<CalculatorAPI.UserInformation> call, Response<CalculatorAPI.UserInformation> response) {
+                Log.d("test", "onResponse: " + response.body());
             }
 
             @Override
-            public void onFailure(Call<CalculatorAPI.Result> call, Throwable t) {
-
+            public void onFailure(Call<CalculatorAPI.UserInformation> call, Throwable t) {
+                Log.d("test", "onFailure: ");
             }
         });
+
+//        call.enqueue(new Callback<CalculatorAPI.Result>() {
+//
+//            @Override
+//            public void onResponse(Call<CalculatorAPI.Result> call, Response<CalculatorAPI.Result> response) {
+//                String result = response.body().getResult();
+//                Log.d("TRY", "onResponse: " + response.body().toString());
+//                calculatorListener.onSuccess(result);
+//            }
+//
+//            @Override
+//            public void onFailure(Call<CalculatorAPI.Result> call, Throwable t) {
+//                Log.d("TRY", "onFailure: " + t.toString());
+//            }
+//        });
     }
 
 
--- app/src/main/java/com/workshop/calculator/network/CalculatorAPI.java
+++ app/src/main/java/com/workshop/calculator/network/CalculatorAPI.java
@@ -2,7 +2,9 @@ package com.workshop.calculator.network;
 
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -13,6 +15,36 @@ public interface CalculatorAPI {
             @Query("first_number") int firstNumber,
             @Query("second_number") int secondNumber);
 
+    @POST("user/{user_id}")
+    Call<UserInformation> getByUser(@Path("user_id") int userId,
+                                    @Body MyRequest myRequest);
+
+
+    public class MyRequest {
+        private String username;
+        private String password;
+
+        public MyRequest(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
 
     public class Result {
         private String result;
@@ -21,6 +53,27 @@ public interface CalculatorAPI {
             return result;
         }
         
+    }
+
+    public class UserInformation {
+        @Override
+        public String toString() {
+            return "UserInformation{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+
+        private int id;
+        private String name;
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 
 }
